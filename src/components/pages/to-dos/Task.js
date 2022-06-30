1757a09a6073ab19ca3e9e8919386f938b373513
mahfuzswaron <mahfuzswaron@gmail.com>
2022-06-30 22:01:28 +0600

    completing done

--- src/components/pages/to-dos/Task.js
+++ src/components/pages/to-dos/Task.js
@@ -23,9 +23,20 @@ const Task = ({ task }) => {
             submit();
         }
     }
+    const handleComplete = () => {
+        fetch(`http://localhost:5000/complete`, {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json',
+                id: task._id
+            }
+        })
+            .then(res => res.json())
+            .then(data => console.log(data));
+    }
     return (
         <li className='flex space-x-4 lg:space-x-8 items-center my-3'>
-            <input type="radio" name="task" class="radio radio-primary" />
+            <input type="radio" name="task" class="radio radio-primary" onClick={handleComplete} />
             <input type="text" onChange={handleChange} onKeyUp={handleChange} onBlur={submit} value={taskText} class="input  w-full " />
         </li>
     );
